737afefa53bd540f71df76070f5891a2eb151ac7
TUAN ANH <TUANANH@ApplenoMacBook-Pro.local>
2021-01-20 22:00:34 +0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import unsplash from '../api/unsplash';
+
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
+class App extends Component {
+
+  state = {
+    images: [],
+  }
+
+  onSearchSubmit = async (term) => {
+    console.log(term);
+    // const response = await unsplash.get('/search/photos', {
+    //   params: { query: term },
+    // });
+    // this.setState({ images: response.data.results });
+  }
+
+  render() {
+    const { images } = this.state;
+    return (
+      <div className="ui container" style={{ marginTop: '20px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={images} />
+      </div>
+    );
+  }
+
+}
+
+export default App;
